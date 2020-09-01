d3b5c4eda0ba12c985f27a8a4ae31321b3bdfe46
Robert Emsbach <robert.emsbach@camunda.com>
2020-09-01 15:13:37 +0800

    Improved example with conditions and moveToLastGateway

--- src/main/java/ModelCreator.java
+++ src/main/java/ModelCreator.java
@@ -3,6 +3,7 @@ import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.FlowElement;
 import org.camunda.bpm.model.bpmn.instance.Gateway;
+import org.camunda.bpm.model.bpmn.instance.UserTask;
 
 import java.io.File;
 
@@ -14,26 +15,28 @@ public class ModelCreator {
 
         BpmnModelInstance modelInst;
         try {
-//        File file = new File(ModelModifier.class.getClassLoader().getResource("testDiagram.bpmn").toURI());
-            File file = new File("/dev/bpmnmodifier/src/main/resources/twitterQA_gen.bpmn");
-            //modelInst = Bpmn.readModelFromFile(file);
+        // File file = new File(ModelModifier.class.getClassLoader().getResource("process1.bpmn").toURI());
+            File file = new File("./src/main/resources/process1.bpmn");
+        //  modelInst = Bpmn.readModelFromFile(file);
             modelInst = Bpmn.createProcess()
                     .name("Twitter QA")
                     .executable()
                     .startEvent()
                     .userTask().id("ApproveTweet").name("Approve Tweet")
                     .exclusiveGateway().id("isApproved").name("Approved?")
+                    .condition("approved", "#{approved}")
                     .serviceTask().id("sendTweet").name("Send tweet")
                     .endEvent().name("Tweet sent")
-                    .done();
-
-            Gateway gateway = modelInst.getModelElementById("isApproved");
-            gateway.builder()
+                    .moveToLastGateway()
+                    // done();
+                    // Gateway gateway = modelInst.getModelElementById("isApproved");
+                    // gateway.builder()
+                    .condition("Not approved", "#{!approved}")
                     .serviceTask().name("Send Rejection")
-                    .endEvent().name("Punished");
+                    .endEvent().name("Tweet rejected").done();
 
             log.info("Flow Elements - Name : Id : Type Name");
-            modelInst.getModelElementsByType(FlowElement.class).forEach(e -> log.info("{} : {} : {}", e.getName(), e.getId(), e.getElementType().getTypeName()));
+            modelInst.getModelElementsByType(UserTask.class).forEach(e -> log.info("{} : {} : {}", e.getName(), e.getId(), e.getElementType().getTypeName()));
 
             Bpmn.writeModelToFile(file, modelInst);
 //        file.createNewFile("/tmp/testDiagram2.bpmn")
